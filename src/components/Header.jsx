92bb165fa82583e8e8065f31c83ae51e052563bb
zironaranjo <129893885+zironaranjo@users.noreply.github.com>
2023-09-26 13:00:09 +0200

    Un trabajo Maravilloso y Honesto

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,8 @@ const Header = () => {
    
     return (
        <h1 className="text-5xl  text-center sm:w-1/2 md:w-2/3 mx-auto">
-         Clientes{''} <span className="text-yellow-300">ZiroRadioActivo</span>
+         Clientes{''} Con Github Ziro{''} 
+         <span className="text-yellow-300">ZiroRadioActivo</span>
          </h1>
      )
    }
